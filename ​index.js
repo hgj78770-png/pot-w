522e859bff9854d5a6f24e8724fba10e25b1ebb8
hgj78770-png <hgj78770@gmail.com>
2026-06-14 23:21:19 +0300

    Create ​index.js

---
+++ ​index.js
@@ -0,0 +1,14 @@
+{
+  "name": "whatsapp-bot",
+  "version": "1.0.0",
+  "description": "WhatsApp Flawe Bot",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.5.0",
+    "qrcode-terminal": "^0.12.0",
+    "@hapi/boom": "^10.0.1"
+  }
+}
